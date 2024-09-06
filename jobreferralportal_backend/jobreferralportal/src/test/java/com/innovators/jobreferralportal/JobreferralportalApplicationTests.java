6d3e1b653353b1f028f3cec1a6c94f15f4b1089a
ShivaSaiReddyKodandapuram <ShivaKodandapuram@my.unt.edu>
2024-09-05 19:59:17 -0500

    added backend module

---
+++ jobreferralportal_backend/jobreferralportal/src/test/java/com/innovators/jobreferralportal/JobreferralportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovators.jobreferralportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobreferralportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
