b8a32c078f276bec217731acc45d5460b587fb01
ravali-kudaravalli24 <ravalikudaravalli@my.unt.edu>
2024-11-11 17:29:44 -0600

    search for employee and HR

--- jobreferralportal_frontend/src/app/Core/employee-dashboard/employee-dashboard.component.ts
+++ jobreferralportal_frontend/src/app/Core/employee-dashboard/employee-dashboard.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../../Services/services/employee.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { JobService } from '../../Services/services/job.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-employee-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './employee-dashboard.component.html',
   styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
+  searchQuery: string = '';
   jobs: any[] = [];
 
-  constructor(private employeeService: EmployeeService, private router: Router) {}
+  constructor(private employeeService: EmployeeService, private router: Router,private jobService: JobService) {}
 
   ngOnInit(): void {
     this.loadJobs();
@@ -35,6 +38,16 @@ export class EmployeeDashboardComponent implements OnInit {
 
   referCandidate(jobId: number): void {
     this.router.navigate(['/refer', jobId]);
+  }
+
+  searchJobs(): void {
+    if (this.searchQuery) {
+      this.jobService.searchJobs(this.searchQuery).subscribe((data: any[]) => {
+        this.jobs = data;
+      });
+    } else {
+      this.loadJobs();
+    }
   }
 
 }
