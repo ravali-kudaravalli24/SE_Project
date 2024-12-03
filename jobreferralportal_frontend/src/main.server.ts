193da07b580119b7baa0b23a9248bc280e3024a0
ShivaSaiReddyKodandapuram <ShivaKodandapuram@my.unt.edu>
2024-12-02 18:51:23 -0600

    disable ssr

--- jobreferralportal_frontend/src/main.server.ts
+++ jobreferralportal_frontend/src/main.server.ts
@@ -1,11 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideServerRendering } from '@angular/platform-server';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { AppComponent } from './app/app.component';
+// import { provideServerRendering } from '@angular/platform-server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, {
-  providers: [
-    provideServerRendering(),
-  ]
-});
+// const bootstrap = () => bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideServerRendering(),
+//   ]
+// });
 
-export default bootstrap;
+// export default bootstrap;
+
+
+export {};
